181381307544d9c3a30102028a3240d6514ebda3
adrianhelllo <hetessyadrian@gmail.com>
2026-04-26 13:03:22 +0200

    change file name and add cli argument count checking

---
+++ bmshoe.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+const char SYMBOLS = {'.', '-', '+', '+'};
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        printf("Correct usage: ./bmshoe file.bmp\n");
+    }
+}
