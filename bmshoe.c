70f6a17a97370e0d02a46a75019ef34e45bcee3e
adrianhelllo <hetessyadrian@gmail.com>
2026-04-29 21:13:44 +0200

    add switch cases for handling option flags

--- bmshoe.c
+++ bmshoe.c
@@ -3,22 +3,56 @@
 #include <unistd.h>
 
 const char SYMBOLS[] = {'.', '-', '=', '+', '%', '&', '#', '@'};
-char *OPTS = "?w:h:";
+
+typedef struct
+{
+    char flagopt;
+    char* description;
+    int has_val; // 0 or 1
+} optflag;
+
+optflag OPTINFO[] = {
+    {'?', "Lists all available options for the program", 0},
+    {'h', "Specifies a desired display height for the image to be shown", 1},
+    {'w', "Specifies a desired display width for the image to be show.", 1},
+};
+
+char *OPTS = ":?w:h:";
+
 
 int main(int argc, char** argv)
 {
     // Ensure correct usage
     int opt;
 
-    while ((opt = getopt(argc, argv, OPTS) != -1))
+    while ((opt = getopt(argc, argv, OPTS)) != -1)
     {
         switch (opt) {
-            case '?':
-                
-            
-            default:
-                printf("Correct usage: ./bmshoe file.bmp {opt1, opt2, ...}. Use the --help flag for more information.\n");
+            case ':':
+                printf("Missing value for -%c\n", optopt);
                 return 1;
+
+            case '?':
+                if (optopt == '?')
+                {
+                    for (int i = 0, len = sizeof(OPTINFO) / sizeof(OPTINFO[0]); i < len; i++)
+                    {
+                        optflag cur_opt = OPTINFO[i];
+
+                        printf("-%c: %s. ", cur_opt.flagopt, cur_opt.description);
+
+                        if (cur_opt.has_val) {printf("Option must take a value.\n");}
+                        else {printf("Option does not take a value.\n");}
+                    }
+                }
+                else
+                {
+                    printf("Unrecognised option. Use the -? option for more information.\n");
+                }
+                return 2;
+            default:
+                printf("Correct usage: ./bmshoe [opt1, opt2, ...] file.bmp. Use the -? option for more information.\n");
+                return 3;
         }
     }
         
